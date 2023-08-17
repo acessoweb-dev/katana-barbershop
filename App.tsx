5eccf0151d6eb92fe4f6e924f541341bb80c8483
Henrique Garcia <henriquegarcia.dev@gmail.com>
2023-08-17 07:48:37 -0300

    Setup inicial do Styled-Components no projeto

--- App.tsx
+++ App.tsx
@@ -1,20 +1,7 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { defaultTheme } from 'styles/default';
+
+import { ThemeProvider } from 'styled-components';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+  return <ThemeProvider theme={defaultTheme}>{/* <Routes /> */}</ThemeProvider>;
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ src/styles/commonStyles.ts
@@ -0,0 +1,34 @@
+import styled from 'styled-components/native';
+
+import { SafeAreaView } from 'react-native-safe-area-context';
+import { KeyboardAvoidingView, View } from 'react-native';
+import { KeyboardAwareScrollView } from 'react-native-keyboard-aware-scroll-view';
+
+// ----------- CONTAINER ------------
+
+export const ContainerPage = styled.View`
+  flex: 1;
+  gap: 15px;
+`;
+
+export const Container = styled(SafeAreaView)`
+  flex: 1;
+  background-color: ${({ theme }) => theme.background};
+`;
+
+// ------------ CONTENT ------------
+
+export const ContentPage = styled.View`
+  justify-content: center;
+  align-items: center;
+  padding: 0px 15px;
+  margin: 15px 0px;
+`;
+
+export const Content = styled(KeyboardAvoidingView)`
+  flex: 1;
+`;
+
+export const ContentScroll = styled(KeyboardAwareScrollView)`
+  flex: 1;
+`;
