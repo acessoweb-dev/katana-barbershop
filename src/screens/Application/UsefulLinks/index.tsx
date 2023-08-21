1c1f532f115cc80d5b7ce8f97f2f26d8a1349a5e
Henrique Garcia <henriquegarcia.dev@gmail.com>
2023-08-21 08:58:53 -0300

    Composição da tela de Links Úteis

--- src/screens/Application/UsefulLinks/index.tsx
+++ src/screens/Application/UsefulLinks/index.tsx
@@ -5,13 +5,44 @@ import BackHeader from 'screens/_headers/Back';
 import { Text } from 'react-native';
 
 import { Props } from 'interfaces/routes.interface';
+import { homeIcon, userIcon } from 'assets';
+import { Image } from 'react-native';
+
+const mockLinks = [
+  {
+    linkId: 'link-01',
+    linkIcon: homeIcon,
+    linkLabel: 'Instagram',
+  },
+  {
+    linkId: 'link-02',
+    linkIcon: userIcon,
+    linkLabel: 'Avalie-nos',
+  },
+  {
+    linkId: 'link-40',
+    linkIcon: '',
+    linkLabel: 'Outro',
+  },
+];
 
 const UsefulLinks = ({ route, navigation }: Props) => {
   return (
     <S.UsefulLinksContainer>
       <BackHeader title="Links Úteis" />
       <S.UsefulLinksContent>
-        <Text>UsefulLinks</Text>
+        <S.UsefulLinksListing>
+          {mockLinks.map(link => (
+            <S.UsefulLink key={link.linkId}>
+              {!!link.linkIcon && (
+                <S.LinkStyles.IconContainer>
+                  <S.LinkStyles.Icon source={link.linkIcon} />
+                </S.LinkStyles.IconContainer>
+              )}
+              <S.LinkStyles.Label>{link.linkLabel}</S.LinkStyles.Label>
+            </S.UsefulLink>
+          ))}
+        </S.UsefulLinksListing>
       </S.UsefulLinksContent>
     </S.UsefulLinksContainer>
   );
--- src/screens/Application/UsefulLinks/styles.ts
+++ src/screens/Application/UsefulLinks/styles.ts
@@ -1,6 +1,40 @@
-import styled from 'styled-components/native';
+import styled, { DefaultTheme } from 'styled-components/native';
 import { ContainerPage, ContentScroll } from 'styles/commonStyles';
 
 export const UsefulLinksContainer = styled(ContainerPage)``;
 
-export const UsefulLinksContent = styled(ContentScroll)``;
+export const UsefulLinksContent = styled(ContentScroll)`
+  padding: 0 20px;
+`;
+
+export const UsefulLinksListing = styled.View`
+  row-gap: 25px;
+`;
+
+export const UsefulLink = styled.TouchableOpacity`
+  position: relative;
+  flex-direction: row;
+  align-items: center;
+  padding: 15px 20px;
+  border-radius: 100px;
+  background-color: ${({ theme }) => theme.primary};
+`;
+
+export const LinkStyles = {
+  IconContainer: styled.View`
+    position: absolute;
+    left: 30px;
+  `,
+  Icon: styled.Image`
+    width: 25px;
+    height: 25px;
+  `,
+  Label: styled.Text`
+    flex: 1;
+    font-family: 'Agrandir';
+    font-size: ${({ theme }: { theme: DefaultTheme }) => theme.fonts.size[18]};
+    letter-spacing: 3px;
+    text-align: center;
+    color: ${({ theme }) => theme.inputs.tertiary.background};
+  `,
+};
